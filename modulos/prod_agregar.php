f0e64c4523c881360fa122c5ad84b0555a71c25b
cetecom <cetecom@LC08L1215>
2017-05-11 13:01:11 -0300

    Segunda clase ... casi listo

---
+++ modulos/prod_agregar.php
@@ -0,0 +1,21 @@
+<?php
+
+include '../lib/conexion.php';
+
+$conn=new Conexion;
+
+if ($conn->conectar()){
+$nom=$_POST["nombre"];
+$totusd=$_POST["totalusd"];
+$ano=$_POST["ano"];
+
+$sql="INSERT INTO productos(nombre,totalusd,ano) values('$nom','$totalusd','$ano')";
+       echo $sql;
+}
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
