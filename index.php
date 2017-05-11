b09237d7bde41a3d81892c90f781e63ace0cebd3
cetecom <cetecom@LC08L1215>
2017-05-11 12:51:55 -0300

    Segunda clase ... casi casi

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
+        <form method="post" action="modulos/prod_agregar.php"
         <?php
         // put your code here
         ?>
